5c005485999327024116a8a28adb1ff74eae9eb6
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2023-04-17 00:11:46 +0530

    enabled ig and nat

--- main.tf
+++ main.tf
@@ -38,21 +38,21 @@ resource "aws_internet_gateway" "ig" {
 }
 
 # Elastic-IP (eip) for NAT
-# resource "aws_eip" "nat_eip" {
-#   vpc        = true
-#   depends_on = [aws_internet_gateway.id]
-# }
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
 
-# # NAT
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
 
-#   tags = {
-#     Name        = "nat"
-#     Environment = "${var.environment}"
-#   }
-# }
+  tags = {
+    Name        = "nat"
+    Environment = "${var.environment}"
+  }
+}
 
 # Public subnet
 resource "aws_subnet" "public_subnet" {
