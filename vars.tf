d80a6c505308129061f8fddcd435a9f97bc0eb68
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2023-04-16 22:09:19 +0530

    terrform init

---
+++ vars.tf
@@ -0,0 +1,33 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "environment" {
+  default = "javatodev-dev"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  description = "CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = list
+  default = ["10.0.0.0/20", "10.0.128.0/20"]
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list
+  default = ["10.0.16.0/20", "10.0.144.0/20"]
+  description = "CIDR block for Private Subnet"
+}
+
+variable "availability_zones" {
+  type        = list
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+  description = "AZ in which all the resources will be deployed"
+}
