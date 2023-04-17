4ce466a5cc2a87c29f36b3e67fa7a098dba1d5f9
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2023-04-17 18:58:56 +0530

    main tf

--- main.tf
+++ main.tf
@@ -255,6 +255,7 @@ resource "aws_lb" "app_load_balancer" {
   depends_on = [
     aws_s3_bucket.load_balancer_log
   ]
+  enable_cross_zone_load_balancing = "true"
   security_groups = [aws_security_group.public.id]
   subnets         = [for subnet in aws_subnet.public_subnet : subnet.id]
   access_logs {
@@ -280,12 +281,25 @@ resource "aws_lb_listener" "app_load_balancer" {
   ]
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_tg.id
   }
+}
+
+resource "aws_lb_target_group_attachment" "app_server" {
+  target_group_arn = aws_lb_target_group.lb_tg.arn
+  count            = length(aws_instance.app_server)
+  target_id        = element(aws_instance.app_server.*.id, count.index)
+  port             = 8080
+}
+
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "${var.environment}-lb-target-group"
+  health_check {
+    path = "/actuator/health"
+    port = 8080
+  }
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
 }
